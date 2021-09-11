9f101d6b00e7e3862c8513c85224343ec633db9c
Chipzstar <chipzstar.dev@googlemail.com>
2021-09-11 22:22:29 +0100

    add seconds api key generation

--- helpers/auth.js
+++ helpers/auth.js
@@ -1,13 +1,14 @@
 const db = require("../models/index");
 const jwt = require("jsonwebtoken");
 const path = require("path");
+const crypto = require("crypto");
 
-exports.login = async (req, res, next) => {
+const login = async (req, res, next) => {
 	try {
 		let user = await db.User.findOne({
 			email: req.body.email
 		});
-		let { _id, firstname, lastname, email, company, createdAt } = user;
+		let {_id, firstname, lastname, email, company, createdAt, apiKey } = user;
 		let isMatch = await user.comparePassword(req.body.password);
 		if (isMatch) {
 			let token = jwt.sign({
@@ -19,7 +20,7 @@ exports.login = async (req, res, next) => {
 				process.env.SECRET_KEY
 			);
 			return res.status(200).json({
-				id: _id, firstname, lastname, email, company, createdAt, token,
+				id: _id, firstname, lastname, email, company, createdAt, token, apiKey,
 				message: "You have logged in Successfully!"
 			});
 		} else {
@@ -37,12 +38,12 @@ exports.login = async (req, res, next) => {
 	}
 };
 
-exports.register = async (req, res, next) => {
-	console.log("req.file:", req.file);
+const register = async (req, res, next) => {
+	console.log("req.body:", req.body);
 	try {
 		//create a user
-		let user = await db.User.create(req.file ? { ...req.body, profileImageURL: req.file.path } : { ...req.body });
-		let { id, firstname, lastname, email, company, createdAt } = user;
+		let user = await db.User.create(req.file ? {...req.body, profileImageURL: req.file.path} : {...req.body});
+		let {id, firstname, lastname, email, company, createdAt, apiKey} = user;
 		//create a jwt token
 		let token = jwt.sign({
 				id,
@@ -59,7 +60,9 @@ exports.register = async (req, res, next) => {
 			email,
 			createdAt,
 			company,
-			token
+			apiKey,
+			token,
+			message: "New user registered successfully!"
 		});
 	} catch (err) {
 		//if validation fails!
@@ -73,3 +76,49 @@ exports.register = async (req, res, next) => {
 		});
 	}
 };
+
+const generateSecurityKeys = async (req, res, next) => {
+	// generate the apiKey using random byte sequences
+	try {
+		const { email } = req.body;
+		const rand = crypto.randomBytes(24);
+		let chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789".repeat(2)
+		let apiKey = '';
+
+		for (let i = 0; i < rand.length; i++) {
+			let index = rand[i] % chars.length;
+			apiKey += chars[index];
+		}
+		console.log("Generated API Key", apiKey);
+		// update the user's details with the new api key
+		await db.User.findOneAndUpdate({ email }, { "apiKey": apiKey }, { new: true });
+		// return the new apiKey
+		return res.status(201).json({
+			apiKey
+		});
+	} catch (err) {
+		return next({
+			status: 400,
+			message: err.message
+		})
+	}
+}
+
+const updateProfile = async (req, res, next) => {
+	try {
+		const { id, ...params } = req.body;
+		console.log(params)
+		const user  = db.User.findByIdAndUpdate(id, params, { new: true})
+		console.log("User", user)
+		return res.status(200).json({
+			message: "Profile updated successfully!"
+		})
+	} catch (err) {
+		return next({
+			status: 400,
+			message: err.message
+		})
+	}
+}
+
+module.exports = { register, login, generateSecurityKeys, updateProfile }
--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 require("dotenv").config();
 const express = require("express");
-const app = express();
+const moment = require('moment-timezone');
+moment.tz.setDefault("Europe/London");
 const logger = require('morgan');
 const cors = require("cors");
 const bodyParser = require('body-parser');
@@ -8,6 +9,8 @@ const errorHandler = require('./helpers/error');
 const authRoutes = require('./routes/auth');
 const shopifyRoutes = require('./routes/shopify');
 const { authorizeUser, authenticateUser } = require('./middleware/auth');
+
+const app = express();
 app.use(logger("dev"))
 
 const db = require('./models/index');
@@ -16,14 +19,14 @@ const PORT = process.env.PORT || 8081;
 app.use(cors())
 app.use(bodyParser.json())
 
-app.get('/api', (req, res) => {
+app.get('/server', (req, res) => {
     res.status(200).json({
         message: "Welcome to Shopify!"
     })
 })
 app.use('/uploads', express.static('uploads'));
-app.use('/api/auth', authRoutes);
-app.use('/api/shopify', shopifyRoutes);
+app.use('/server/auth', authRoutes);
+app.use('/server/shopify', shopifyRoutes);
 app.use(
     authenticateUser,
     authorizeUser
