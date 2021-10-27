f9bb6b15ef514bc0b333bf6b43fa59f6028d2667
Chipzstar <chipzstar.dev@googlemail.com>
2021-10-27 13:56:38 +0100

    configure body-parser for stripe webhook

--- routes/stripe.js
+++ routes/stripe.js
@@ -1,28 +1,25 @@
-const express = require("express");
-const {handleActiveSubscription, handleCanceledSubscription} = require("../helpers");
+const express = require('express');
+const { handleActiveSubscription, handleCanceledSubscription } = require('../helpers');
 const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
+const bodyParser = require('body-parser');
 const router = express.Router();
 
-router.post('/webhook', express.raw({type: 'application/json'}), (request, response) => {
+router.post('/webhook', bodyParser.raw({ type: 'application/json' }), (request, response) => {
 	let event = request.body;
 	// Replace this endpoint secret with your endpoint's unique secret
 	// If you are testing with the CLI, find the secret by running 'stripe listen'
 	// If you are using an endpoint defined with the API or dashboard, look in your webhook settings
 	// at https://dashboard.stripe.com/webhooks
-	const endpointSecret = process.env.STRIPE_WEBHOOK_SECRET
+	const endpointSecret = process.env.STRIPE_WEBHOOK_SECRET;
 	// Only verify the event if you have an endpoint secret defined.
 	// Otherwise use the basic event deserialized with JSON.parse
-	console.log(endpointSecret)
+	console.log(endpointSecret);
 	if (endpointSecret) {
 		// Get the signature sent by Stripe
 		const signature = request.headers['stripe-signature'];
 		try {
-			event = stripe.webhooks.constructEvent(
-				request.body,
-				signature,
-				endpointSecret
-			);
-			console.log("✅  Success Webhook verified!")
+			event = stripe.webhooks.constructEvent(request.body, signature, endpointSecret);
+			console.log('✅  Success Webhook verified!');
 		} catch (err) {
 			console.log(`⚠  Webhook signature verification failed.`, err.message);
 			return response.sendStatus(400);
@@ -30,9 +27,9 @@ router.post('/webhook', express.raw({type: 'application/json'}), (request, respo
 	}
 	let subscription;
 	let status;
-	console.log("=====================================")
-	console.log(event)
-	console.log("=====================================")
+	console.log('=====================================');
+	console.log(event);
+	console.log('=====================================');
 	// Handle the event
 	switch (event.type) {
 		case 'customer.subscription.trial_will_end':
@@ -48,7 +45,7 @@ router.post('/webhook', express.raw({type: 'application/json'}), (request, respo
 			// Then define and call a method to handle the subscription deleted.
 			handleCanceledSubscription(subscription)
 				.then(res => console.log(res))
-				.catch(err => console.error(err))
+				.catch(err => console.error(err));
 			break;
 		case 'customer.subscription.created':
 			subscription = event.data.object;
@@ -57,24 +54,24 @@ router.post('/webhook', express.raw({type: 'application/json'}), (request, respo
 			// Then define and call a method to handle the subscription created.
 			break;
 		case 'customer.subscription.updated':
-			console.log("CUSTOMER_SUBSCRIPTION_UPDATED")
+			console.log('CUSTOMER_SUBSCRIPTION_UPDATED');
 			subscription = event.data.object;
 			status = subscription.status;
 			handleActiveSubscription(subscription)
 				.then(res => console.log(res))
-				.catch(err => console.error(err))
+				.catch(err => console.error(err));
 			break;
 		case 'invoice.paid':
-			console.log("INVOICE_PAID")
+			console.log('INVOICE_PAID');
 			break;
 		case 'payment_intent.succeeded':
-			console.log("PAYMENT_INTENT_SUCCEEDED")
+			console.log('PAYMENT_INTENT_SUCCEEDED');
 			break;
 		case 'payment_intent.created':
-			console.log("PAYMENT_INTENT_CREATED")
+			console.log('PAYMENT_INTENT_CREATED');
 			break;
 		case 'invoice.payment_succeeded':
-			console.log("INVOICE_PAYMENT_SUCCEEDED")
+			console.log('INVOICE_PAYMENT_SUCCEEDED');
 			break;
 		default:
 			// Unexpected event type
