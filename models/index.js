995d3a2843e1ea67082e114b0fa6f6ba7785a77b
Chipzstar <chipzstar.dev@googlemail.com>
2021-11-02 14:58:58 +0000

    add subscription-status handling to customer.subscription.created event

--- models/index.js
+++ models/index.js
@@ -1,6 +1,4 @@
 const mongoose = require('mongoose')
-const Grid = require('gridfs-stream')
-Grid.mongo = mongoose.mongo
 
 mongoose.set("debug", false);
 mongoose.Promise = Promise;
@@ -12,16 +10,6 @@ mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost:27017/seconds",
     useFindAndModify: false,
     useUnifiedTopology: true
 }, () => console.log("Connected to Mongo Database!"))
-
-let gfs;
-const conn = mongoose.connection;
-
-conn.once('open', () => {
-    //Initialise the stream
-    console.log("opening connection...")
-    gfs = Grid(conn.db)
-    gfs.collection('uploads')
-})
 
 module.exports.User = mongoose.model('User', require("./user"));
 module.exports.PostCode = mongoose.model("Postcode", require("./region"));
