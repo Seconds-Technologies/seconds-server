75bd0725aa47b5d6319c3b36cf7b7609e7636bd7
Chipzstar <chipzstar.dev@googlemail.com>
2021-11-06 23:37:34 +0000

    add endpoint for updating deliveryHours

---
+++ models/deliveryHoursSchema.js
@@ -0,0 +1,63 @@
+const mongoose = require('mongoose');
+
+const openSchema = new mongoose.Schema({
+	h: {
+		type: Number,
+		default: 7
+	},
+	m: {
+		type: Number,
+		default: 30
+	}
+})
+
+const closeSchema = new mongoose.Schema({
+	h: {
+		type: Number,
+		default: 18
+	},
+	m: {
+		type: Number,
+		default: 0
+	}
+})
+
+const hoursSchema = new mongoose.Schema({
+	open: {
+		type: openSchema
+	},
+	close: {
+		type: closeSchema
+	},
+	canDeliver:{
+		type: Boolean,
+		default: true
+	},
+});
+
+
+const deliveryHoursSchema = new mongoose.Schema({
+	0: {
+		type: hoursSchema,
+	},
+	1: {
+		type: hoursSchema,
+	},
+	2: {
+		type: hoursSchema,
+	},
+	3: {
+		type: hoursSchema,
+	},
+	4: {
+		type: hoursSchema,
+	},
+	5: {
+		type: hoursSchema,
+	},
+	6: {
+		type: hoursSchema,
+	}
+})
+
+module.exports = { deliveryHoursSchema, hoursSchema };
