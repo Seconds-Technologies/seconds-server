592fef26ce9b6a962f5f1bb19cc9190b524fef79
Chipzstar <chipzstar.dev@googlemail.com>
2021-11-23 14:49:44 +0000

    set up weekly commission fee subscription

--- helpers/index.js
+++ helpers/index.js
@@ -1,5 +1,6 @@
 const { S3 } = require('../constants/index');
 const db = require('../models');
+const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
 
 function encode(data) {
 	let buf = Buffer.from(data);
@@ -10,7 +11,7 @@ async function getBase64Image(filename) {
 	if (filename) {
 		const data = await S3.getObject({
 			Bucket: 'seconds-profile-pictures',
-			Key: filename,
+			Key: filename
 		}).promise();
 		console.log(data);
 		return encode(data.Body);
@@ -18,29 +19,12 @@ async function getBase64Image(filename) {
 	return '';
 }
 
-function filterOrders(orders, postcodes){
-	const filtered = orders.filter((item => {
-		let postcode = item["shipping_address"]["zip"]
-		//console.log(postcode, postcode.replace(/\s/g, ""));
-		return postcodes.some(prefix => postcode.startsWith(prefix) || postcode.replace(/\s/g, "").startsWith(prefix))
-	})).map(item => ({ ...item, status: "NEW" }))
-	console.log("Num filtered orders: ", filtered.length);
-	return filtered
-}
-
-function updateStatuses(shopifyOrders, DBOrders) {
-	return shopifyOrders.map(item => {
-		let dbOrder = DBOrders.find(order => order.id === item.id)
-		return dbOrder === undefined ? item : { ...item, status: dbOrder["status"]}
-	})
-}
-
 function updateOrders(orders, id, status) {
 	let index = orders.findIndex(o => o.id === id);
 	let order = orders[index];
-	let newOrder = {...order, status}
+	let newOrder = { ...order, status };
 	orders.splice(index, 1, newOrder);
-	return orders
+	return orders;
 }
 
 function filterAndUpdateProducts(products, id, keys) {
@@ -48,20 +32,20 @@ function filterAndUpdateProducts(products, id, keys) {
 	//filter product by id
 	let index = products.findIndex(p => p.id === id);
 	let product = products[index];
-	if ("status" in keys) {
-		product = updateStatus(product, keys["status"]);
+	if ('status' in keys) {
+		product = updateStatus(product, keys['status']);
 	}
-	if ("quantity" in keys) {
-		product = updateStock(product, keys["quantity"]);
+	if ('quantity' in keys) {
+		product = updateStock(product, keys['quantity']);
 	}
 	products.splice(index, 1, product);
 	return products;
 }
 
-function filterAndRemove(products, id){
+function filterAndRemove(products, id) {
 	let index = products.findIndex(p => p.id === id);
 	products.splice(index, 1);
-	return products
+	return products;
 }
 
 function updateStatus(product, status) {
@@ -70,20 +54,44 @@ function updateStatus(product, status) {
 
 function updateStock(product, quantity) {
 	console.log(quantity);
-	product["variants"][0]["inventory_quantity"] = quantity;
+	product['variants'][0]['inventory_quantity'] = quantity;
 	return product;
 }
 
 async function handleActiveSubscription(subscription) {
 	try {
-		console.log(subscription);
+		console.log(subscription.items.data);
+		const SUBSCRIPTION_PLANS = process.env.STRIPE_SUBSCRIPTION_PLANS.split(' ');
+		const COMMISSION_PLANS = process.env.STRIPE_COMMISSION_PLANS.split(' ');
 		const { id, customer, status, items: { data } } = subscription;
 		if (status === 'active') {
-			const user = await db.User.findOneAndUpdate(
-				{ stripeCustomerId: customer },
-				{ subscriptionId: id, subscriptionPlan: data[0].price.lookup_key },
-				{ new: true }
-			);
+			let user = null;
+			if (SUBSCRIPTION_PLANS.includes(data[0].price.lookup_key)) {
+				user = await db.User.findOneAndUpdate(
+					{ stripeCustomerId: customer },
+					{ subscriptionId: id, subscriptionPlan: data[0].price.lookup_key },
+					{ new: true }
+				);
+				const prices = await stripe.prices.list({
+					lookup_keys: [`${data[0].price.lookup_key}-commission`],
+					expand: ['data.product']
+				});
+				await stripe.subscriptions.create({
+					customer: user.stripeCustomerId,
+					default_payment_method: user.paymentMethodId,
+					items: [
+						{ price: prices.data[0].id }
+					]
+				});
+			} else if (COMMISSION_PLANS.includes(data[0].price.lookup_key)) {
+				user = await db.User.findOneAndUpdate(
+					{ stripeCustomerId: customer },
+					{ stripeCommissionId: id },
+					{ new: true }
+				);
+			} else {
+				throw new Error(`Unrecognized subscription item with look-up key ${data[0].price.lookup_key}`);
+			}
 			if (user) {
 				console.log('------------------------------------');
 				console.log('updated user:', user);
@@ -97,20 +105,34 @@ async function handleActiveSubscription(subscription) {
 		}
 	} catch (err) {
 		console.error(err);
-		throw err
+		throw err;
 	}
 }
 
 async function handleCanceledSubscription(subscription) {
 	try {
-		console.log(subscription);
-		const { customer, status } = subscription;
+		console.log(subscription.items.data);
+		const SUBSCRIPTION_PLANS = process.env.STRIPE_SUBSCRIPTION_PLANS.split(' ');
+		const COMMISSION_PLANS = process.env.STRIPE_COMMISSION_PLANS.split(' ');
+		const { customer, status, items: { data } } = subscription;
 		if (status === 'canceled') {
-			const user = await db.User.findOneAndUpdate(
-				{ stripeCustomerId: customer },
-				{ subscriptionId: '', subscriptionPlan: '' },
-				{ new: true }
-			);
+			let user = null;
+			if (SUBSCRIPTION_PLANS.includes(data[0].price.lookup_key)) {
+				user = await db.User.findOneAndUpdate(
+					{ stripeCustomerId: customer },
+					{ subscriptionId: '', subscriptionPlan: '', stripeCommissionId: '' },
+					{ new: true }
+				);
+				await stripe.subscriptions.del(user.stripeCommissionId, {
+					invoice_now: true
+				});
+			} else if (COMMISSION_PLANS.includes(data[0].price.lookup_key)) {
+				user = await db.User.findOneAndUpdate(
+					{ stripeCustomerId: customer },
+					{ stripeCommissionId: '' },
+					{ new: true }
+				);
+			}
 			console.log('------------------------------------');
 			console.log('updated user:', user);
 			console.log('------------------------------------');
@@ -126,11 +148,9 @@ async function handleCanceledSubscription(subscription) {
 
 module.exports = {
 	getBase64Image,
-	filterOrders,
 	updateOrders,
 	filterAndRemove,
-	updateStatuses,
 	filterAndUpdateProducts,
 	handleActiveSubscription,
 	handleCanceledSubscription
-}
+};
--- models/user.js
+++ models/user.js
@@ -192,7 +192,10 @@ const userSchema = new mongoose.Schema({
 		type: Date,
 		default: Date.now()
 	},
-	jobs: []
+	jobs: [],
+	stripeCommissionId: {
+		type: String
+	}
 });
 
 userSchema.pre('save', async function (next) {
