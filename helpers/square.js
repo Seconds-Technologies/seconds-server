c2184f5bad2668b9782fab70cf87bf31d47119ac
Chipzstar <chipzstar.dev@googlemail.com>
2021-11-30 18:45:13 +0000

    add square sdk to retrieve square account details

--- helpers/square.js
+++ helpers/square.js
@@ -1,11 +1,26 @@
-const axios = require('axios');
 const db = require('../models/index');
 const { v4: uuidv4 } = require('uuid');
+const axios = require('axios');
+const { Client, Environment } = require('square')
+
+const client = new Client({
+	environment: Environment.Production,
+	accessToken: process.env.SQUARE_ACCESS_TOKEN,
+})
 
 const getCredentials = async (req, res) => {
 	try {
-		console.log(req.body);
-		res.status(200).json(req.body);
+		console.table(req.body);
+		const { clientId, clientSecret } = req.body.credentials;
+		// const URL = "https://connect.squareup.com/oauth2/token"
+		const payload = {
+			clientId,
+			clientSecret,
+			code: req.body.code,
+			grantType: 'authorization_code'
+		}
+		const { result } = await client.oAuthApi.obtainToken(payload)
+		res.status(200).json({ clientId, clientSecret, accessToken: result.accessToken, shopId: result.merchantId, domain: "", country: "" });
 	} catch (err) {
 		console.error(err);
 		res.status(400).json({ message: err.message });
@@ -14,7 +29,7 @@ const getCredentials = async (req, res) => {
 
 const authorizeSquareAccount = async (req, res, next) => {
 	try {
-		const { clientId, clientSecret, code } = req.body;
+		const { clientId } = req.body;
 		const baseURL = "https://connect.squareup.com"
 		const scope = "ORDERS_READ+MERCHANT_PROFILE_READ+PAYMENTS_READ+SETTLEMENTS_READ+BANK_ACCOUNTS_READ+INVENTORY_READ+CUSTOMERS_READ"
 		const state = uuidv4()
