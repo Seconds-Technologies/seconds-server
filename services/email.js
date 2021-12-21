22b0ae12840c63731be654d1c577e38c56f57eb2
Chipzstar <chipzstar.dev@googlemail.com>
2021-12-21 20:39:57 +0000

    fix - package dependencies conflicts remove nodemailer and replace with sendgrid/mail package

--- services/email.js
+++ services/email.js
@@ -1,41 +1,19 @@
-const nodemailer = require('nodemailer');
-const nodemailerSendGrid = require('nodemailer-sendgrid');
+const sgMail = require('@sendgrid/mail');
+
+sgMail.setApiKey(process.env.SENDGRID_API_KEY);
 
 const sendEmail = async options => {
-	// 1) Create a transporter
-	const transporter = nodemailer.createTransport(
-		nodemailerSendGrid({
-			apiKey: process.env.SENDGRID_API_KEY,
-		})
-		// Activate in gmail "less secure app" option
-	);
-
-	/*const transporterTest =  nodemailer.createTransport({
-		host: process.env.EMAIL_HOST,
-		port: process.env.EMAIL_PORT,
-		auth: {
-			user: process.env.MAILTRAP_SMTP_USERNAME,
-			pass: process.env.MAILTRAP_SMTP_PASSWORD
-		}
-	})*/
-
-	// 2) definer the email options
-	const mailOptions = {
+	const msg = {
 		from: 'Seconds Technologies <ola@useseconds.com>',
-		to: `${options.full_name} <${options.email}>`,
+		to: `${options.name} <${options.email}>`,
 		subject: options.subject,
-		...(options.message && {text: options.message}),
-		...(options.html && {html: options.html})
+		...(options.message && { text: options.message }),
+		...(options.html && { html: options.html }),
+		...(options.templateId && { templateId: options.templateId }),
+		...(options.templateData && { dynamicTemplateData: options.templateData })
 	};
-
-	/*const mailOptionsTest = {
-		from: 'Seconds Technologies <test@example.com>',
-		to: `${options.full_name} <${options.email}>`,
-		subject: options.subject,
-		text: options.message
-	}*/
 	// 3) Actually send the email
-	process.env.ENVIRONMENT_MODE === "live" && await transporter.sendMail(mailOptions);
+	process.env.ENVIRONMENT_MODE === "live" && await sgMail.send(msg);
 };
 
 module.exports = sendEmail;
