2d2ae6ee23e69529dbc210a8b82d7c80ba8f9dd1
Chipzstar <chipzstar.dev@googlemail.com>
2021-12-21 20:39:57 +0000

    fix - package dependencies conflicts remove nodemailer and replace with sendgrid/mail package

--- models/index.js
+++ models/index.js
@@ -5,10 +5,6 @@ mongoose.Promise = Promise;
 
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost:27017/seconds", {
     keepAlive: true,
-    useCreateIndex: true,
-    useNewUrlParser: true,
-    useFindAndModify: false,
-    useUnifiedTopology: true
 }, () => console.log("Connected to Mongo Database!"))
 
 module.exports.User = mongoose.model('User', require("./user"));
