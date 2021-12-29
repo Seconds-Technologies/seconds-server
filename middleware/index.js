34fe7eed729d02445bb92aafff8d3a6f2eaabb59
Chipzstar <chipzstar.dev@googlemail.com>
2021-12-29 14:49:43 +0000

    bug fix redirect for timeout

--- middleware/index.js
+++ middleware/index.js
@@ -1,6 +1,6 @@
-const wooCommerceTimeout = (req, res, next) => {
-	console.log(req.timedout)
-	!req.timedout && next()
+const wooCommerceTimeout = (err, req, res, next) => {
+	console.log("TIMEOUT", req.timedout)
+	!req.timedout && res.redirect(303, `${process.env.CLIENT_HOST}/integrate/woocommerce?success=0&error=${err.message}`);
 }
 
 module.exports = { wooCommerceTimeout };
