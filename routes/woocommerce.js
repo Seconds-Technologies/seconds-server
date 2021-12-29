f35d9f6bbca0c6106f35cee2243d0f665ed0b9d3
Chipzstar <chipzstar.dev@googlemail.com>
2021-12-29 13:26:40 +0000

    add callback Url endpoint for woocommerce authorization

--- routes/woocommerce.js
+++ routes/woocommerce.js
@@ -1,7 +1,8 @@
 const express = require("express");
-const { authorizeWoocommerceAccount } = require('../helpers/woocommerce');
+const { getCredentials, authorizeWoocommerceAccount } = require('../helpers/woocommerce');
 const router = express.Router();
 
+router.get('/', getCredentials)
 router.post("/authorize", authorizeWoocommerceAccount);
 
 module.exports = router;
