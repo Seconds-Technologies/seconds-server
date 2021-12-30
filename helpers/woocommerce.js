a455c65056f072a836a03c57c62bfb2fe0fab968
Chipzstar <chipzstar.dev@googlemail.com>
2021-12-30 12:56:45 +0000

    add endpoint for fetching woocommerce details

--- helpers/woocommerce.js
+++ helpers/woocommerce.js
@@ -1,13 +1,9 @@
 const db = require('../models/index');
 const { v4: uuidv4 } = require('uuid');
 const querystring = require('querystring');
-const axios = require('axios');
 
 const authorizeWoocommerceAccount = async (req, res, next) => {
 	try {
-		/*setTimeout(() => {
-			res.redirect(304, `${process.env.CLIENT_HOST}/integrate/woocommerce?success=0&error=Request timeout`);
-		}, 5000)*/
 		const { store_url, email } = req.body;
 		const endpoint = '/wc-auth/v1/authorize';
 		const params = {
@@ -15,7 +11,7 @@ const authorizeWoocommerceAccount = async (req, res, next) => {
 			scope: 'read',
 			user_id: email,
 			return_url: `${process.env.CLIENT_HOST}/integrate/woocommerce`,
-			callback_url: `${process.env.SERVER_HOST}/server/woocommerce`
+			callback_url: `${process.env.SERVER_HOST}/server/woocommerce/callback`
 		}
 		// trim off any trailing slashes
 		let domain = store_url.endsWith('/') ? store_url.slice(0, -1) : store_url;
@@ -37,14 +33,33 @@ const authorizeWoocommerceAccount = async (req, res, next) => {
 	}
 };
 
-const getCredentials = async (req, res) => {
+const getWooCommerceDetails = async (req, res, next) => {
 	try {
-		const { user_id, key_id, consumer_key, consumer_secret } = req.body;
+		let { email } = req.query
+		let { woocommerce } = await db.User.findOne({ email });
+		if (woocommerce.consumerSecret || woocommerce.consumerKey) {
+			res.status(200).json({
+				...woocommerce,
+			});
+		} else {
+			throw new Error("This user has no woocommerce account integrated");
+		}
+	} catch (err) {
+		console.error(err);
+		return next({
+			status: 404,
+			message: "Unable to retrieve woocommerce details",
+		});
+	}
+}
+
+const callback = async (req, res) => {
+	try {
+		const { user_id, consumer_key, consumer_secret } = req.body;
 		const user = await db.User.findOneAndUpdate({ 'email': user_id }, {
 			'woocommerce.consumerKey': consumer_key,
 			'woocommerce.consumerSecret': consumer_secret
 		}, { new: true });
-		console.log(user.woocommerce);
 		const URL = `${user['woocommerce'].domain}/wp-json/wc/v3/system_status`;
 		/*const { environment } = (await axios.get(URL, {
 			auth: {
@@ -62,4 +77,4 @@ const getCredentials = async (req, res) => {
 	}
 };
 
-module.exports = { getCredentials, authorizeWoocommerceAccount };
+module.exports = { callback, getWooCommerceDetails, authorizeWoocommerceAccount };
