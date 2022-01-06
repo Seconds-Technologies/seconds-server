55117f09628d506bbaeb073a80f12d99af75cd39
Chipzstar <chipzstar.dev@googlemail.com>
2022-01-06 13:37:00 +0000

    update squarespace connect endpoint to fetch access token

--- routes/squarespace.js
+++ routes/squarespace.js
@@ -2,7 +2,7 @@ const express = require("express");
 const { connect, authorize } = require('../helpers/squarespace');
 const router = express.Router();
 
-router.post('/connect', connect)
+router.post('/', connect)
 router.post('/authorize', authorize)
 
 module.exports = router;
