054d7ec723bd2bf8f2331fb5ce46e250f3e0ec9f
Chipzstar <chipzstar.dev@googlemail.com>
2022-02-14 13:17:51 +0000

    add update driver endpoint persist drivers for client login remove redundant local db models change clientId to clientIds for driver creation update database schemas v1.0.2 to v1.0.6 add sms service for new driver registration

---
+++ services/sms.js
@@ -0,0 +1,28 @@
+// twilio client
+const accountSid = process.env.TWILIO_ACCOUNT_SID;
+const authToken = process.env.TWILIO_AUTH_TOKEN;
+const TwilioClient = require('twilio')(accountSid, authToken, {
+	logLevel: 'debug'
+});
+const PNF = require('google-libphonenumber').PhoneNumberFormat
+const phoneUtil = require('google-libphonenumber').PhoneNumberUtil.getInstance()
+
+const sendSMS = async (phone, template, alphaSender="Seconds") => {
+	try {
+		const sender = alphaSender ? alphaSender : process.env.TWILIO_SERVICE_NUMBER
+		const number = phoneUtil.parseAndKeepRawInput(phone, 'GB');
+		const E164Number = phoneUtil.format(number, PNF.E164)
+		console.log("E164 Phone Number:", E164Number)
+		process.env.TWILIO_STATUS === 'active' && await TwilioClient.messages.create({
+			body: template,
+			from: sender,
+			to: E164Number
+		});
+		return true
+	} catch (err) {
+		console.error(err);
+		throw err;
+	}
+}
+
+module.exports = sendSMS
