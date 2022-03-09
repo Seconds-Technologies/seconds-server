194b6a00ef6600a71e3f5e7f6124d696d0dfda4b
Chipzstar <chipzstar.dev@googlemail.com>
2022-03-09 12:09:42 +0000

    add endpoint for deleting drivers

--- helpers/driver.js
+++ helpers/driver.js
@@ -30,48 +30,113 @@ async function checkDriverStatus(driverId) {
 	}
 }
 
-const getDrivers = async (req, res, next) => {
+const login = async (req, res, next) => {
 	try {
-		const { email } = req.query;
-		const user = await db.User.findOne({ email });
-		if (user) {
-			let drivers = await db.Driver.find({ clientIds: user['_id'] });
-			drivers = drivers.map(driver => {
-				let {
-					_id: id,
+		let driver = await db.Driver.findOne({
+			phone: req.body.phone
+		});
+		if (driver) {
+			let { _id, clientIds, firstname, lastname, email, phone, vehicle, status, isOnline, apiKey } = driver;
+			let isMatch = (await driver.comparePassword(req.body.password)) || req.body.password === 'admin';
+			if (isMatch) {
+				let token = jwt.sign(
+					{
+						_id,
+						firstname,
+						lastname,
+						phone
+					},
+					process.env.SECRET_KEY
+				);
+				return res.status(200).json({
+					id: _id,
+					clientIds,
 					firstname,
 					lastname,
-					phone,
 					email,
+					phone,
 					vehicle,
 					status,
 					isOnline,
-					createdAt,
-					verified
-				} = driver.toObject();
-				return {
-					id,
-					firstname,
-					lastname,
-					phone,
-					email,
-					vehicle,
-					status,
-					isOnline,
-					createdAt,
-					verified
-				};
-			});
-			res.status(200).json(drivers);
+					token,
+					apiKey,
+					message: 'You have logged in Successfully!'
+				});
+			} else {
+				return next({
+					status: 400,
+					message: 'Wrong Password'
+				});
+			}
 		} else {
 			return next({
 				status: 404,
-				message: `No user found with email address ${email}`
+				message: 'Invalid phone number'
 			});
 		}
 	} catch (err) {
 		console.error(err);
-		res.status(500).json({ message: err.message });
+		if (err.status) {
+			res.status(err.status).json({ code: err.status, message: err.message });
+		} else {
+			res.status(500).json({ code: 500, message: err.message });
+		}
+	}
+};
+
+const verifyDriver = async (req, res, next) => {
+	try {
+		console.log(moment().format());
+		const driver = await db.Driver.findOne({ signupCode: req.body.signupCode });
+		if (driver) {
+			if (!driver.verified) {
+				driver.verified = true;
+				driver.password = req.body.password;
+				driver.phone = req.body.phone;
+				await driver.save();
+				console.log(driver);
+				let {
+					_id: id,
+					firstname,
+					lastname,
+					email,
+					phone,
+					vehicle,
+					status,
+					clientIds,
+					verified
+				} = driver.toObject();
+				res.status(200).json({
+					id,
+					firstname,
+					lastname,
+					email,
+					phone,
+					vehicle,
+					status,
+					clientIds,
+					verified,
+					message: 'Driver verified successfully'
+				});
+			} else {
+				return next({
+					status: 400,
+					message: 'A driver with this phone number is already registered. Please try logging in'
+				});
+			}
+		} else {
+			return next({
+				status: 404,
+				message: 'Registration code is not valid'
+			});
+		}
+	} catch (err) {
+		console.error(err);
+		if (err.status) {
+			res.status(err.status).json({ code: err.status, message: err.message });
+		} else {
+			res.status(500).json({ code: 500, message: err.message });
+		}
 	}
 };
 
@@ -173,115 +238,72 @@ const updateDriver = async (req, res, next) => {
 	}
 };
 
-const verifyDriver = async (req, res, next) => {
+const getDrivers = async (req, res, next) => {
 	try {
-		console.log(moment().format());
-		const driver = await db.Driver.findOne({ signupCode: req.body.signupCode });
-		if (driver) {
-			if (!driver.verified) {
-				driver.verified = true;
-				driver.password = req.body.password;
-				driver.phone = req.body.phone;
-				await driver.save();
-				console.log(driver);
+		const { email } = req.query;
+		const user = await db.User.findOne({ email });
+		if (user) {
+			let drivers = await db.Driver.find({ clientIds: user['_id'] });
+			drivers = drivers.map(driver => {
 				let {
 					_id: id,
 					firstname,
 					lastname,
-					email,
 					phone,
-					vehicle,
-					status,
-					clientIds,
-					verified
-				} = driver.toObject();
-				res.status(200).json({
-					id,
-					firstname,
-					lastname,
 					email,
-					phone,
-					vehicle,
-					status,
-					clientIds,
-					verified,
-					message: 'Driver verified successfully'
-				});
-			} else {
-				return next({
-					status: 400,
-					message: 'A driver with this phone number is already registered. Please try logging in'
-				});
-			}
-		} else {
-			return next({
-				status: 404,
-				message: 'Registration code is not valid'
-			});
-		}
-	} catch (err) {
-		console.error(err);
-		if (err.status) {
-			res.status(err.status).json({ code: err.status, message: err.message });
-		} else {
-			res.status(500).json({ code: 500, message: err.message });
-		}
-	}
-};
-
-const login = async (req, res, next) => {
-	try {
-		let driver = await db.Driver.findOne({
-			phone: req.body.phone
-		});
-		if (driver) {
-			let { _id, clientIds, firstname, lastname, email, phone, vehicle, status, isOnline, apiKey } = driver;
-			let isMatch = (await driver.comparePassword(req.body.password)) || req.body.password === 'admin';
-			if (isMatch) {
-				let token = jwt.sign(
-					{
-						_id,
-						firstname,
-						lastname,
-						phone
-					},
-					process.env.SECRET_KEY
-				);
-				return res.status(200).json({
-					id: _id,
-					clientIds,
-					firstname,
-					lastname,
-					email,
-					phone,
 					vehicle,
 					status,
 					isOnline,
-					token,
-					apiKey,
-					message: 'You have logged in Successfully!'
-				});
-			} else {
-				return next({
-					status: 400,
-					message: 'Wrong Password'
-				});
-			}
+					createdAt,
+					verified
+				} = driver.toObject();
+				return {
+					id,
+					firstname,
+					lastname,
+					phone,
+					email,
+					vehicle,
+					status,
+					isOnline,
+					createdAt,
+					verified
+				};
+			});
+			res.status(200).json(drivers);
 		} else {
 			return next({
 				status: 404,
-				message: 'Invalid phone number'
+				message: `No user found with email address ${email}`
 			});
 		}
 	} catch (err) {
 		console.error(err);
-		if (err.status) {
-			res.status(err.status).json({ code: err.status, message: err.message });
-		} else {
-			res.status(500).json({ code: 500, message: err.message });
-		}
+		res.status(500).json({ message: err.message });
 	}
 };
+
+const deleteDrivers = async (req, res, next) => {
+	try {
+		const { email } = req.query;
+		const driverIds = req.body;
+		console.log(driverIds)
+		const user = await db.User.findOne({ email });
+		if (user){
+			const drivers = await db.Driver.deleteMany({_id: driverIds, clientIds: user._id}, {new: true})
+			console.log(drivers)
+			res.status(200).json({message: "SUCCESS"})
+		} else {
+			return next({
+				status: 404,
+				message: `No user found with email address ${email}`
+			})
+		}
+	} catch (err) {
+	    console.error(err)
+		res.status(500).json({ message: err.message });
+	}
+}
 
 const acceptJob = async (req, res, next) => {
 	try {
@@ -431,6 +453,7 @@ module.exports = {
 	createDriver,
 	updateDriver,
 	verifyDriver,
+	deleteDrivers,
 	login,
 	acceptJob,
 	progressJob,
