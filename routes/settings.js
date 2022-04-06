da87d4a7a47e37816419bdf0ee2cf1b2f530803a
Chipzstar <chipzstar.dev@googlemail.com>
2022-04-06 15:14:30 +0100

    update input to sqs

--- routes/settings.js
+++ routes/settings.js
@@ -65,7 +65,7 @@ async function createDailyBatchScheduler(isEnabled=false, user, settings) {
 			{
 				Arn: process.env.AWS_SQS_ARN,
 				Id: settings._id,
-				Input: JSON.stringify({ id: user._id })
+				Input: user._id
 			}
 		]
 	});
