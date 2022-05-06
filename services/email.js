4d3e23842836d6e081032aa6363e5d770b6edb46
Chipzstar <chipzstar.dev@googlemail.com>
2022-05-06 10:07:37 +0100

    remove required policy from fullAddress prop for new users cleanup sendEmail service

--- services/email.js
+++ services/email.js
@@ -2,18 +2,22 @@ const sgMail = require('@sendgrid/mail');
 
 sgMail.setApiKey(process.env.SENDGRID_API_KEY);
 
-const sendEmail = async options => {
-	const msg = {
-		from: 'Seconds Technologies <ola@useseconds.com>',
-		to: `${options.name} <${options.email}>`,
-		subject: options.subject,
-		...(options.message && { text: options.message }),
-		...(options.html && { html: options.html }),
-		...(options.templateId && { templateId: options.templateId }),
-		...(options.templateData && { dynamicTemplateData: options.templateData })
-	};
-	// 3) Actually send the email
-	process.env.SENDGRID_STATUS === "active" && await sgMail.send(msg);
+const sendEmail = async (options, canSend = true) => {
+	try {
+		const msg = {
+			from: 'Seconds Technologies <ola@useseconds.com>',
+			to: `${options.name} <${options.email}>`,
+			subject: options.subject,
+			...(options.message && { text: options.message }),
+			...(options.html && { html: options.html }),
+			...(options.templateId && { templateId: options.templateId }),
+			...(options.templateData && { dynamicTemplateData: options.templateData })
+		};
+		// 3) Actually send the email
+		process.env.SENDGRID_STATUS === "active" && canSend && await sgMail.send(msg);
+	} catch (err) {
+		throw err
+	}
 };
 
 module.exports = sendEmail;
