f53d9b6e2318966ab6930430d7d195d9843d193c
Chipzstar <chipzstar.dev@googlemail.com>
2022-05-25 13:03:15 +0100

    update sync endpoint

--- helpers/main.js
+++ helpers/main.js
@@ -190,7 +190,7 @@ const synchronizeUserInfo = async (req, res, next) => {
 				subscriptionId,
 				subscriptionPlan,
 				lastLogin
-			} = user;
+			} = user.toObject();
 			// lookup integrated drivers
 			let drivers = await db.Driver.find({ clientIds: _id });
 			drivers.sort((a, b) => b.createdAt - a.createdAt);
